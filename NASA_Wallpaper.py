c81c6e640bfbe07ad56ed9bafbe534b52794c79b
DylanNadan <143483233+DylanNadan@users.noreply.github.com>
2026-09-08 15:56:24 +1000

    Added comments to the program

--- NASA_Wallpaper.py
+++ NASA_Wallpaper.py
@@ -9,8 +9,13 @@ APOD_API = "https://api.nasa.gov/planetary/apod"
 
 def main():
     """ Main Function """
+    # Get the Wallpaper from the APOD API
     wallpaper_path = get_wallpaper()
+
+    # Fit it to the given screen
     get_fit_wallpaper(wallpaper_path, 1920,1080)
+
+    # Set the wallpaper as the background for the computer
     set_wallpaper(wallpaper_path)
    
 
@@ -26,6 +31,7 @@ def get_wallpaper():
         "thumbs": True
     }
 
+    # Call the APOD API
     response = requests.get(APOD_API, params=params, timeout = 8)
     response.raise_for_status()
 
@@ -41,6 +47,7 @@ def get_wallpaper():
     apod_image_response = requests.get(apod_url, timeout = 30)
     apod_image_response.raise_for_status()
     
+    # Download the image and set the filename
     os.makedirs(os.path.dirname(wallpaper_path), exist_ok = True)
 
     with open(wallpaper_path,"wb") as file:
@@ -51,6 +58,7 @@ def get_wallpaper():
 
 def set_wallpaper(wallpaper_path):
     """Sets the image given at a path to be the wallpaper for the computer"""
+    # Windows archaic variables necessary for setting the change
     wallpaper_action = 20
     update_user_profile = 0x01
     notify_change = 0x02
@@ -66,12 +74,14 @@ def set_wallpaper(wallpaper_path):
 
 def get_fit_wallpaper(wallpaper_path, screen_width, screen_height):
     """ This function refits the image so that it is 1920 by 1080 for images of obscure resolutions"""
+    # This function will overwrite the original file so a copy is necessary since the image will be opened
     with Image.open(wallpaper_path) as img:
         wallpaper = img.copy()
 
-    
+    # Use LANCZOS calculations to scale down the image to 1080p
     wallpaper.thumbnail((screen_width,screen_height), Image.Resampling.LANCZOS)
 
+    # Create the background image and set it to all black
     background = Image.new(
         "RGB",
         (screen_width,screen_height),
@@ -83,6 +93,7 @@ def get_fit_wallpaper(wallpaper_path, screen_width, screen_height):
     center_x = (screen_width - wallpaper.width)//2
     center_y = (screen_height - wallpaper.height)//2
 
+    # Put the wallppaer on top of the black background
     background.paste(wallpaper, (center_x,center_y))
 
     background.save(wallpaper_path)
